6b22c9936e4f59b0befbcd124e54c7a4aa99db5e
Lukas Kouril <kouril.lukas@gmail.com>
2025-05-13 16:59:49 +0000

    setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 
 export default function Home() {
   return (
-    <>
-    <section>
+    <div className="flex flex-col">
+    <section className="bg-purple-500 font-sans p-2 my-2">
       <p>What Goes Up Must Come Down</p>
     </section>
-    <section>
+    <section className="bg-green-200 font-serif text-black p-2 mb-2">
     <p>Down to Earth</p>
   </section>
-  <section>
+  <section className="bg-rose-400 font-mono p-2">
   <p>Every cloud has a silver lining</p>
 </section>
-</>
+</div>
   );
 }
